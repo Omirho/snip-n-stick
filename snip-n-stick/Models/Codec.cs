7f60835ef4cb8662e645b05d89aba739c336d24d
Nishant Gupta <nish.gupta01@gmail.com>
2015-04-05 16:58:36 +0530

    Codec

---
+++ snip-n-stick/Models/Codec.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace snip_n_stick.Models
+{
+    public class Codec
+    {
+
+        public static readonly string Alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+        public static readonly int Base = Alphabet.Length;
+
+        public static string Encode(int i)
+        {
+            if (i == 0) return Alphabet[0].ToString();
+
+            i += 100000;
+
+            var s = string.Empty;
+
+            while (i > 0)
+            {
+                s += Alphabet[i % Base];
+                i = i / Base;
+            }
+
+            return string.Join(string.Empty, s.Reverse());
+        }
+
+        public static int Decode(string s)
+        {
+            var i = 0;
+
+            foreach (var c in s)
+            {
+                i = (i * Base) + Alphabet.IndexOf(c);
+            }
+
+            i -= 100000;
+
+            return i;
+        }
+
+    }
+}
--- snip-n-stick/view.aspx.cs
+++ snip-n-stick/view.aspx.cs
@@ -12,9 +12,6 @@ namespace snip_n_stick
     public partial class WebForm1 : System.Web.UI.Page
     {
 
-        public static readonly string Alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
-        public static readonly int Base = Alphabet.Length;
-
         protected void Page_Load(object sender, EventArgs e)
         {
         }
@@ -22,42 +19,12 @@ namespace snip_n_stick
         public IQueryable<Snip> GetSnip([RouteData] string snipid)
         {
             var _db = new snip_n_stick.Models.SnipContext();
-            int id = Decode(snipid);
+            int id = Codec.Decode(snipid);
             IQueryable<Snip> query = _db.Snips;
             query = query.Where(p => p.SnipID == id);
             
             return query;
         }
 
-        public static string Encode(int i)
-        {
-            if (i == 0) return Alphabet[0].ToString();
-
-            i += 100000;
-
-            var s = string.Empty;
-
-            while (i > 0)
-            {
-                s += Alphabet[i % Base];
-                i = i / Base;
-            }
-
-            return string.Join(string.Empty, s.Reverse());
-        }
-
-        public static int Decode(string s)
-        {
-            var i = 0;
-
-            foreach (var c in s)
-            {
-                i = (i * Base) + Alphabet.IndexOf(c);
-            }
-
-            i -= 100000;
-
-            return i;
-        }
     }
 }
