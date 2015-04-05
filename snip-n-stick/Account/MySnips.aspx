09e97f85333b6986dac8820f1c7bc8651efd2c8b
Pulkit Arora <pulkitarora_26@yahoo.com>
2015-04-05 16:29:45 +0530

    getmysnips implemented

--- snip-n-stick/Account/MySnips.aspx
+++ snip-n-stick/Account/MySnips.aspx
@@ -1,3 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MySnips.aspx.cs" Inherits="snip_n_stick.Account.MySnips" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+    </p>
 </asp:Content>
--- snip-n-stick/Account/MySnips.aspx.cs
+++ snip-n-stick/Account/MySnips.aspx.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Owin;
 using System.Web.UI.WebControls;
+using snip_n_stick.Models;
 
 namespace snip_n_stick.Account
 {
@@ -11,7 +17,15 @@ namespace snip_n_stick.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        public IQueryable<Snip> GetMySnips()
+        {
+            var _db = new SnipContext();
+            IQueryable<Snip> query = _db.Snips;
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            query = query.Where(p => p.SnipCreatedBy == manager.GetEmail(User.Identity.GetUserId()));
+            return query;
         }
     }
 }
